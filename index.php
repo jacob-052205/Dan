6d09abb39f829a39085527fab312fcc4a9cb9f90
jacob-052205 <jerimayajahuran@gmail.com>
2026-05-23 12:32:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>CI/CD Jacob Demo</title>
+    <title>CI/CD Demo with PHP</title>
 </head>
 <body>
-    <h1>Hello from Jenkins CI/CD Pipeline! Jacob</h1>
+    <h1>Hello from Jenkins CI/CD Pipeline!</h1>
     <p>This page was deployed automatically from the "Dan" repository.</p>
+    <p>Current time: <?php echo date('Y-m-d H:i:s'); ?></p>
 </body>
 </html>
